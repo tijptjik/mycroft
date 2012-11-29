5acf8c706bd4acb36ef6f831c61e62ec893047fa
Mart van de Ven <m@type.hk>
2012-11-29 23:41:55 +0800

    Added new pages 'story', 'testimonials' and 'store'

--- mycroft/base/urls.py
+++ mycroft/base/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.defaults import url, patterns
 
 urlpatterns = patterns('mycroft.base.views',
     url(r'^$', 'index', name='index'),
+    url(r'^store/$', 'store', name='store'),
     url(r'^lecture/(?P<poet_last_name>\w+)/(?P<poem_title>[\w\s\d\.\-]+)/?$', 'lecture', name='lecture'),
     url(r'^order/(?P<order_reference>[\w\d]+)/', 'order', name='order'),
+    url(r'^story/$', 'story', name='story'),
+    url(r'^testimonials/$', 'testimonials', name='testimonials'),
     url(r'^contact/$', 'contact', name='contact'),
-    url(r'^about/$', 'about', name='about'),
 )
