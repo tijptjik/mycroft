27b34e1d41007d3cd9435b8b82010457c73e810f
Mart van de Ven <m@type.hk>
2012-11-29 23:40:32 +0800

    Prepopulate slug

--- mycroft/base/admin.py
+++ mycroft/base/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from models import *
 
-for model in [Product, Video, Lecturer, Series, Poet, Poem, Lecture, Testimonial]:
+for model in [Product, Video, Lecturer, Series, Poet, Poem, Testimonial, Preview]:
      admin.site.register(model)
+
+
+class LectureAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Lecture, LectureAdmin)
